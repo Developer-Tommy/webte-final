63228d5e88db0a498952071f53df3d77bde12d63
Developer-Tommy <knapcoktomas@gmail.com>
2022-05-18 16:00:42 +0200

    new com

---
+++ web/public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Controller\PersonController;
+
+include '../app/vendor/autoload.php';
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+    <script src="./js/script.js"></script>
+
+  <style>
+      table, tr, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+      }
+  </style>
+
+</head>
+
+<body>
+    <h1>final zadanie</h1>
+
+    <?php 
+      $output = "";
+      //exec('octave-cli --eval "m1 = 2500; m2 = 320;k1 = 80000; k2 = 500000;b1 = 350; b2 = 15020;pkg load control;A=[0 1 0 0;-(b1*b2)/(m1*m2) 0 ((b1/m1)*((b1/m1)+(b1/m2)+(b2/m2)))-(k1/m1) -(b1/m1);b2/m2 0 -((b1/m1)+(b1/m2)+(b2/m2)) 1;k2/m2 0 -((k1/m1)+(k1/m2)+(k2/m2)) 0];B=[0 0;1/m1 (b1*b2)/(m1*m2);0 -(b2/m2);(1/m1)+(1/m2) -(k2/m2)];C=[0 0 1 0]; D=[0 0];Aa = [[A,[0 0 0 0]\'];[C, 0]];Ba = [B;[0 0]];Ca = [C,0]; Da = D;K = [0 2.3e6 5e8 0 8e6];sys = ss(Aa-Ba(:,1)*K,Ba,Ca,Da);t = 0:0.01:5;r =0.1;initX1=0; initX1d=0;initX2=0; initX2d=0;[y,t,x]=lsim(sys*[0;1],r*ones(size(t)),t,[initX1;initX1d;initX2;initX2d;0]);save out.txt t"', $output);
+      exec('octave-cli --eval "1+1"', $output);
+      echo $output[0];
+    ?>
+</body>
+</html>
